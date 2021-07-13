5cb01cbafa57d98b2c0083e91c4900e4ce02194f
mominahmed23 <mominahmad324@gmail.com>
2021-07-14 00:21:28 +0500

    cors issue handled

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 let port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/bbtest-auth", (req, res) => {
   res.status(405).json({ error: "GET method not supported." });
